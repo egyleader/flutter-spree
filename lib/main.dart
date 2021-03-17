f3a7b279bc752356f0a227b0564a6dbb41ca5467
Ahmed Osama <ahmed.os.attya@gmail.com>
2021-03-17 21:24:59 +0200

    multi language & RTL  support with easy localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:spree/utils/helpers/env.dart';
 import 'package:spree/utils/services/prefrences.dart';
+import 'package:easy_localization/easy_localization.dart';
 
 void main() async {
   await WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
   await Env.init(); // loading environmental values
-  await Prefs.init();
-  runApp(MyApp());
+  await Prefs.init(); // init SharedPrefrences
+  runApp(EasyLocalization(
+    supportedLocales: [Locale('en', 'US'), Locale('ar', 'EG')],
+    path: 'assets/translations',
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -15,6 +22,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       title: 'Flutter Spree App',
       home: MyHomePage(),
     );
@@ -22,21 +32,35 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
+    bool _localeSwitch = context.locale.languageCode == 'en' ? false : true;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter Spree App'),
+        title: Text(tr('title')),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'Api key :  ' + Env.load('API_KEY').toString(),
-            ),
+            Text( tr('temp.api') + ':  ' + Env.load('API_KEY').toString()),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text( tr('temp.current_locale') +' :  ' + context.locale.languageCode),
+                Switch.adaptive(
+                    value: _localeSwitch,
+                    onChanged: (value) {
+                      print(value);
+                      _localeSwitch = value;
+                      print(_localeSwitch);
+                      value
+                          ? context.setLocale(Locale('ar', 'EG'))
+                          : context.setLocale(Locale('en', 'US'));
+                    })
+              ],
+            )
           ],
         ),
       ),
